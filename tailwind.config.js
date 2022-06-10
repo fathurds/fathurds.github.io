674e71cc2391fc747e5d82e623314a6cad4374cb
Fathurrachman D. Saputra <namasayafathur@gmail.com>
2022-06-10 23:36:25 +0700

    Hero section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        dark: '#334155'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
